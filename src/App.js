cc969785567473f905b1e8b08e39b282ba423fa6
Bo Bartolome <bo.bartolome@proton.me>
2023-02-21 03:25:07 -1000

    add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import SeedColors from './SeedColors';
 import { generatePalette } from './ColorHelpers';
@@ -6,10 +7,19 @@ import { generatePalette } from './ColorHelpers';
 class App extends Component{
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(SeedColors[4])} />
-      </div>
-    )
+      <Switch>
+        <Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE!</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>INDIVIDUAL PALETTE!</h1>}
+        />
+      </Switch>
+
+      //   <div>
+      //     <Palette palette={generatePalette(SeedColors[4])} />
+      //   </div>
+    );
   }
 }
 
