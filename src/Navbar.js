72701731c25b889b75fb5fe83faf90836bbf4acc
Bo Bartolome <bo.bartolome@proton.me>
2023-02-24 03:49:13 -1000

    add Navbar and Footer to SingleColorPalette

--- src/Navbar.js
+++ src/Navbar.js
@@ -27,14 +27,15 @@ class Navbar extends Component {
     }
 
     render() {
-        const { level, changeLevel } = this.props
+        const { level, changeLevel, showingAllColors } = this.props
         const { format } = this.state;
         return (
           <header className="Navbar">
             <div className="logo">
               <Link to='/'>ReactColorPicker</Link>
             </div>
-            <div className="slider-container">
+            {this.props.showingAllColors && (
+              <div className="slider-container">
               <span>Level: {level}</span>
               <div className="slider">
                 <Slider
@@ -46,6 +47,7 @@ class Navbar extends Component {
                 />
               </div>
             </div>
+            )}
             <div className="select-container">
               <Select value={format} onChange={this.handleFormatChange}>
                 <MenuItem value="hex">HEX - #ffffff</MenuItem>
